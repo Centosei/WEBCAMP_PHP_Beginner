6dda8fd2c434b0134434d921ed627fc8a32bc9ae
centosei <centoseisigarette@gmail.com>
2022-10-06 06:58:57 +0000

    my code of bit_operation.php

---
+++ bit_operation.php
@@ -0,0 +1,18 @@
+<?php
+
+$i = random_int(0, 255);
+$j = random_int(0, 255);
+
+echo "i is {$i} and j is {$j} \n";
+// AND
+$k = $i & $j;
+printf("%x & %x = %x \n", $i, $j, $k);
+// OR
+$k = $i | $j;
+printf("%x | %x = %x \n", $i, $j, $k);
+// XOR
+$k = $i ^ $j;
+printf("%x ^ %x = %x \n", $i, $j, $k);
+// NOT
+$k = ~$i;
+printf("~%x = %x \n", $i, $k);
