735e61f39205432b8b74374d306557ef596d4a0c
centosei <centoseisigarette@gmail.com>
2022-10-06 23:51:33 +0000

    i wrote a php code to understand how to compare things

---
+++ greater_lesser.php
@@ -0,0 +1,36 @@
+<?php
+
+$age = 20;
+echo "when the age is {$age} \n";
+// 超える　/　未満
+var_dump($age > 20);
+var_dump($age < 20);
+
+// 以上　/　以下
+var_dump($age >= 20);
+var_dump($age <= 20);
+
+// 19
+
+$age = 19;
+echo "when the age is {$age} \n";
+// 超える　/　未満
+var_dump($age > 20);
+var_dump($age < 20);
+
+// 以上　/　以下
+var_dump($age >= 20);
+var_dump($age <= 20);
+
+
+// 21
+
+$age = 21;
+echo "when the age is {$age} \n";
+// 超える　/　未満
+var_dump($age > 20);
+var_dump($age < 20);
+
+// 以上　/　以下
+var_dump($age >= 20);
+var_dump($age <= 20);
