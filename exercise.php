5eafab0241a4f1224ef9e69fe8e93012d8d706ab
centosei <centoseisigarette@gmail.com>
2022-10-06 07:05:40 +0000

    I finished an exercise for chapter 3

---
+++ exercise.php
@@ -0,0 +1,13 @@
+<?php
+
+$price = 1800;
+$tax = .1;
+
+$price_with_tax = $price * ($tax + 1);
+echo $price_with_tax, "\n";
+
+$last_name = "sei";
+$first_name = "Cento";
+
+$name = $first_name . $last_name . "\n";
+echo $name;
